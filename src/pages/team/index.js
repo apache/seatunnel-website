25c87b058a48b1b4b46a3db9d4ea0f6a269e9f73
benjobs <benjobs@qq.com>
2021-12-28 23:50:18 +0800

    navbar i18n support.

--- src/pages/team/index.js
+++ src/pages/team/index.js
@@ -14,6 +14,24 @@ export default function () {
             <div className="block team_page">
                 <h3 className="team_title">SeaTunnel Team</h3>
                 <p className="team_desc" dangerouslySetInnerHTML={{__html: dataSource.info.desc}}></p>
+                <h3 className="team_title">Mentor</h3>
+                <p className="team_desc">{dataSource.info.tip}</p>
+                <ul className="character_list">
+                    {
+                        dataSource.mentor.map((item, i) => (
+                            <a href={'https://github.com/' + item.githubId} key={i} target="_blank">
+                                <li className="character_item text_center" style={{'listStyle': 'none'}}>
+                                    <img className="character_avatar" src={item.avatarUrl} alt={item.name}/>
+                                    <div className="character_desc">
+                                        <h3 className="character_name">{item.name}</h3>
+                                        <h3 className="character_id"><span className="githubId">githubId:</span>{item.githubId}</h3>
+                                    </div>
+                                </li>
+                            </a>
+                        ))
+                    }
+                </ul>
+
                 <h3 className="team_title">PPMC</h3>
                 <p className="team_desc">{dataSource.info.tip}</p>
                 <ul className="character_list">
@@ -31,43 +49,6 @@ export default function () {
                         ))
                     }
                 </ul>
-
-                <h3 className="team_title">PMC</h3>
-                <p className="team_desc">{dataSource.info.tip}</p>
-                <ul className="character_list">
-                    {
-                        dataSource.pmc.map((item, i) => (
-                            <a href={'https://github.com/' + item.githubId} key={i} target="_blank">
-                                <li className="character_item text_center" style={{'listStyle': 'none'}}>
-                                    <img className="character_avatar" src={item.avatarUrl} alt={item.name}/>
-                                    <div className="character_desc">
-                                        <h3 className="character_name">{item.name}</h3>
-                                        <h3 className="character_id"><span className="githubId">githubId:</span>{item.githubId}</h3>
-                                    </div>
-                                </li>
-                            </a>
-                        ))
-                    }
-                </ul>
-
-                <h3 className="team_title">Committer</h3>
-                <p className="team_desc">{dataSource.info.tip}</p>
-                <ul className="character_list">
-                    {
-                        dataSource.committer.map((item, i) => (
-                            <a href={'https://github.com/' + item.githubId} key={i} target="_blank">
-                                <li className="character_item text_center" style={{'listStyle': 'none'}}>
-                                    <img className="character_avatar" src={item.avatarUrl} alt={item.name}/>
-                                    <div className="character_desc">
-                                        <h3 className="character_name">{item.name}</h3>
-                                        <h3 className="character_id"><span className="githubId">githubId:</span>{item.githubId}</h3>
-                                    </div>
-                                </li>
-                            </a>
-                        ))
-                    }
-                </ul>
-
             </div>
         </Layout>
     );
