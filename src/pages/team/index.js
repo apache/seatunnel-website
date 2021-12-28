6c929d2df538c11752e0ede00dfcffdd13ae87ad
CalvinKirs <kirs@apache.org>
2021-12-28 21:16:03 +0800

    Update Team Page

--- src/pages/team/index.js
+++ src/pages/team/index.js
@@ -14,6 +14,23 @@ export default function () {
             <div className="block team_page">
                 <h3 className="team_title">SeaTunnel Team</h3>
                 <p className="team_desc" dangerouslySetInnerHTML={{__html: dataSource.info.desc}}></p>
+                <h3 className="team_title">Mentor</h3>
+                <p className="team_desc">{dataSource.info.tip}</p>
+                <ul className="character_list">
+                    {
+                        dataSource.mentor.map((item, i) => (
+                            <a href={'https://github.com/' + item.githubId} key={i} target="_blank">
+                                <li className="character_item text_center" style={{'listStyle': 'none'}}>
+                                    <img className="character_avatar" src={item.avatarUrl} alt={item.name}/>
+                                    <div className="character_desc">
+                                        <h3 className="character_name">{item.name}</h3>
+                                    </div>
+                                </li>
+                            </a>
+                        ))
+                    }
+                </ul>
+
                 <h3 className="team_title">PPMC</h3>
                 <p className="team_desc">{dataSource.info.tip}</p>
                 <ul className="character_list">
@@ -31,24 +48,7 @@ export default function () {
                     }
                 </ul>
 
-                <h3 className="team_title">PMC</h3>
-                <p className="team_desc">{dataSource.info.tip}</p>
-                <ul className="character_list">
-                    {
-                        dataSource.pmc.map((item, i) => (
-                            <a href={'https://github.com/' + item.githubId} key={i} target="_blank">
-                                <li className="character_item text_center" style={{'listStyle': 'none'}}>
-                                    <img className="character_avatar" src={item.avatarUrl} alt={item.name}/>
-                                    <div className="character_desc">
-                                        <h3 className="character_name">{item.name}</h3>
-                                    </div>
-                                </li>
-                            </a>
-                        ))
-                    }
-                </ul>
-
-                <h3 className="team_title">Committer</h3>
+               {/* <h3 className="team_title">Committer</h3>
                 <p className="team_desc">{dataSource.info.tip}</p>
                 <ul className="character_list">
                     {
@@ -63,7 +63,7 @@ export default function () {
                             </a>
                         ))
                     }
-                </ul>
+                </ul>*/}
 
             </div>
         </Layout>
