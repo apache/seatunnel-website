b04897fc4afdf93c4235095153b1873981769f4a
benjobs <benjobs@qq.com>
2021-12-29 10:20:10 +0800

    "Core Features" adaptation issue fixed (#25)
    * navbar i18n support.
    
    * navbar i18n support.
    
    * navbar i18n support.
    
    * navbar i18n support.
    
    * githubid issue fixed.

--- src/pages/team/index.js
+++ src/pages/team/index.js
@@ -49,25 +49,6 @@ export default function () {
                         ))
                     }
                 </ul>
-
-               {/* <h3 className="team_title">Committer</h3>
-                <p className="team_desc">{dataSource.info.tip}</p>
-                <ul className="character_list">
-                    {
-                        dataSource.committer.map((item, i) => (
-                            <a href={'https://github.com/' + item.githubId} key={i} target="_blank">
-                                <li className="character_item text_center" style={{'listStyle': 'none'}}>
-                                    <img className="character_avatar" src={item.avatarUrl} alt={item.name}/>
-                                    <div className="character_desc">
-                                        <h3 className="character_name">{item.name}</h3>
-                                        <h3 className="character_id"><span className="githubId">githubId:</span>{item.githubId}</h3>
-                                    </div>
-                                </li>
-                            </a>
-                        ))
-                    }
-                </ul>*/}
-
             </div>
         </Layout>
     );
