f025839158c5a514b05757a53ccbb53fa56dbc9c
Kirs <acm_master@163.com>
2021-12-31 16:35:57 +0800

    Update Homepage (#40)
    * Update HomePage IMAGE
    
    * Update Team Page

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -58,16 +58,9 @@ export default function () {
                     <h1 className="main_subtitle text_center">{dataSource.common.architecture}</h1>
                     <div className="item_block" style={{position: 'relative'}}>
                         <p className="paragraph">{dataSource.home.architecture.desc}</p>
-                        <div className="bold_dot" style={{top: '64px', left: '416px'}}></div>
-                        <div className="bold_dot" style={{top: '700px', left: '240px'}}></div>
-
                         <div className="architecture-image">
                             <img className="button_icon github1" src={useBaseUrl('/image/seatunnel-workflow.svg')} alt="System Architecture"/>
                         </div>
-
-                        <svg width="860" height="860" viewBox="0 0 100 100">
-                            <circle cx="50" cy="50" r="49.8" className="dotted"/>
-                        </svg>
                     </div>
                 </div>
             </div>
