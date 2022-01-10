be177065afeb7b5228556319a6e8843fa47572c8
tigerweili <tigerweili@tencent.net>
2022-01-10 22:19:39 +0800

    add quick start urls

--- src/js/sysConfig.js
+++ src/js/sysConfig.js
@@ -5,5 +5,11 @@ const  systemConfiguration={
         "projectIssueUrl":"https://github.com/apache/incubator-seatunnel/issues",
         "projectPrUrl":"https://github.com/apache/incubator-seatunnel/pulls",
     },
+    doc: {
+        quickStartDocUrls:{
+            'zh-CN': 'https://interestinglab.github.io/seatunnel-docs/#/zh-cn/v2/',
+            'en': 'https://interestinglab.github.io/seatunnel-docs/#/en/'
+        }
+    }
 }
 export  default systemConfiguration
