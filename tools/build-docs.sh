848642fd8877b47793ac5560d5bfd1d7fec83081
wuchunfu <319355703@qq.com>
2022-02-16 09:25:58 +0800

    Fix deploy publish bug (#57)

--- tools/build-docs.sh
+++ tools/build-docs.sh
@@ -1,12 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 WEBSITE_REPO="https://github.com/apache/incubator-seatunnel-website.git"
 MAIN_REPO="https://github.com/apache/incubator-seatunnel.git"
 
+WEBSITE_REPO_NAME="incubator-seatunnel-website"
 WEBSITE_NAME="seatunnel-website"
 MAIN_NAME="seatunnel"
 
-WORK_PATH="/tmp/seatunnel"
+WORK_PATH=~/work/incubator-seatunnel-website
 
 if [ ! -d ${WORK_PATH} ]; then
   mkdir -p ${WORK_PATH}
@@ -25,10 +26,10 @@ echo "===>>>: current work path: ${WORK_PATH}"
 
 echo "===>>>: Clone git repositories"
 
-echo "===>>>: Clone ${MAIN_NAME} repositories"
+echo "===>>>: Clone ${WEBSITE_REPO} repositories to ${WEBSITE_NAME}"
 git clone --depth 1 ${WEBSITE_REPO} ${WORK_PATH}/${WEBSITE_NAME}
 
-echo "===>>>: Clone ${WEBSITE_NAME} repositories"
+echo "===>>>: Clone ${MAIN_REPO} repositories to ${MAIN_NAME}"
 git clone --depth 1 ${MAIN_REPO} ${WORK_PATH}/${MAIN_NAME}
 
 echo "===>>>: Replace elements inside md files"
@@ -36,7 +37,7 @@ cp -rf ${WORK_PATH}/${MAIN_NAME}/docs/en/* ${WORK_PATH}/${WEBSITE_NAME}/docs/
 
 echo "===>>>: Replace docs Done"
 
-echo "===>>>: Copy ${WEBSITE_NAME} to home directory"
-cp -rf ${WORK_PATH}/${WEBSITE_NAME} ~/web
+echo "===>>>: Copy ${WORK_PATH}/${WEBSITE_NAME} to ${WORK_PATH}/${WEBSITE_REPO_NAME} directory"
+cp -rf ${WORK_PATH}/${WEBSITE_NAME} ${WORK_PATH}/${WEBSITE_REPO_NAME}
 
-echo "===>>>: Replace web Done"
+echo "===>>>: Replace ${WORK_PATH}/${WEBSITE_REPO_NAME} Done"
