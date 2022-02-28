549364c5da57cf1c8ab4542aadba9663f7381a48
wuchunfu <319355703@qq.com>
2022-02-28 22:42:10 +0800

    [Chore][Docs] Build documentation script optimization

--- tools/build-docs.sh
+++ tools/build-docs.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -exv
+
 WEBSITE_REPO="https://github.com/apache/incubator-seatunnel-website.git"
 MAIN_REPO="https://github.com/apache/incubator-seatunnel.git"
 
@@ -9,22 +11,10 @@ MAIN_NAME="seatunnel"
 
 WORK_PATH=~/work/${WEBSITE_REPO_NAME}
 
-# To be compatible with MacOS and Linux
-txt=""
-if [[ "$OSTYPE" == "darwin"* ]]; then
-  # Mac OSX
-  txt=''
-elif [[ "$OSTYPE" == "linux-gnu" ]]; then
-  # linux
-  txt=""
-elif [[ "$OSTYPE" == "freebsd"* ]]; then
-  # ...
-  txt=""
-else
-  # Unknown.
-  echo "Operating system unknown, please tell us(submit issue) for better service"
-  exit 1
-fi
+MAIN_PATH=${WORK_PATH}/${MAIN_NAME}
+WEBSITE_PATH=${WORK_PATH}/${WEBSITE_NAME}
+WEBSITE_REPO_PATH=${WORK_PATH}/${WEBSITE_REPO_NAME}
+DOCS_EN=${MAIN_PATH}/docs/en
 
 if [ ! -d ${WORK_PATH} ]; then
   mkdir -p ${WORK_PATH}
@@ -41,23 +31,46 @@ echo "===>>>: current work path: ${WORK_PATH}"
 echo "===>>>: Clone git repositories"
 
 echo "===>>>: Clone ${WEBSITE_REPO} repositories to ${WEBSITE_NAME}"
-git clone --depth 1 ${WEBSITE_REPO} ${WORK_PATH}/${WEBSITE_NAME}
+git clone --depth 1 ${WEBSITE_REPO} ${WEBSITE_PATH}
 
 echo "===>>>: Clone ${MAIN_REPO} repositories to ${MAIN_NAME}"
-git clone --depth 1 ${MAIN_REPO} ${WORK_PATH}/${MAIN_NAME}
+git clone --depth 1 ${MAIN_REPO} ${MAIN_PATH}
 
-echo "===>>>: Copy images to ${WORK_PATH}/${WEBSITE_NAME}/static/doc/image_en/"
-cp -rf ${WORK_PATH}/${MAIN_NAME}/docs/en/images/* ${WORK_PATH}/${WEBSITE_NAME}/static/doc/image_en/
+echo "===>>>: Copy images to ${WEBSITE_PATH}/static/doc/image_en/"
+cp -rf ${DOCS_EN}/images/* ${WEBSITE_PATH}/static/doc/image_en/
+
+if [ -d ${DOCS_EN}/images ]; then
+  rm -rf ${DOCS_EN}/images
+fi
 
 echo "===>>>: Replace images path to /doc/image_en"
-sed -r -i ${txt} "s/(\.\.\/)+images/\/doc\/image_en/g" ${WORK_PATH}/${MAIN_NAME}/docs/en/**/*.md
+function replaceImagesPath(){
+  CURRENT_DIR=$1
+  echo "===>>>: Current directory: ${CURRENT_DIR}"
+  for filePath in ${CURRENT_DIR}/*; do
+    if test -f ${filePath}; then
+      if [ "${filePath##*.}"x = "md"x ]; then
+        echo "===>>: Replace images path to /doc/image_en in ${filePath}"
+        if [[ "$OSTYPE" == "darwin"* ]]; then
+          sed -r -i '' "s/(\.\.\/)+images/\/doc\/image_en/g" ${filePath}
+        else
+          sed -r -i "s/(\.\.\/)+images/\/doc\/image_en/g" ${filePath}
+        fi
+      fi
+    else
+      replaceImagesPath ${filePath}
+    fi
+  done
+}
 
-echo "===>>>: Replace elements inside md files"
-cp -rf ${WORK_PATH}/${MAIN_NAME}/docs/en/* ${WORK_PATH}/${WEBSITE_NAME}/docs/
+replaceImagesPath ${DOCS_EN}
+
+echo "===>>>: Replace elements inside md filePath"
+cp -rf ${DOCS_EN}/* ${WEBSITE_PATH}/docs/
 
 echo "===>>>: Replace docs Done"
 
-echo "===>>>: Copy ${WORK_PATH}/${WEBSITE_NAME} to ${WORK_PATH}/${WEBSITE_REPO_NAME} directory"
-cp -rf ${WORK_PATH}/${WEBSITE_NAME} ${WORK_PATH}/${WEBSITE_REPO_NAME}
+echo "===>>>: Copy ${WEBSITE_PATH} to ${WEBSITE_REPO_PATH} directory"
+cp -rf ${WEBSITE_PATH} ${WEBSITE_REPO_PATH}
 
-echo "===>>>: Replace ${WORK_PATH}/${WEBSITE_REPO_NAME} Done"
+echo "===>>>: Replace ${WEBSITE_REPO_PATH} Done"
