24c2a602c5524f1e2e131fea4fed807dd07a72a7
Kirs <acm_master@163.com>
2022-04-02 10:12:16 +0800

    Update index.jsx (#102)

--- src/pages/download/index.jsx
+++ src/pages/download/index.jsx
@@ -16,7 +16,6 @@ export default function () {
 				<div>
 					<h1>Download the SeaTunnel(Incubating) releases</h1>
 					<p>Use the links below to download the Apache SeaTunnel(Incubating) from one of our mirrors.</p>
-					<p className="tip">Only source code releases are official Apache releases: Windows and Linux binary distributions are just for end user convenience.</p>
 					<h2>SeaTunnel</h2>
 					<table className="version-table">
 						<thead>
