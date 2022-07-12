bbd4fb9167f42ec5f68e4888115c1097e34efc00
labbomb <739955946@qq.com>
2022-07-12 16:59:50 +0800

    feat: Hidden Chinese and English switch

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -179,10 +179,10 @@ const config = {
                     label: 'GitHub',
                     position: 'right',
                 },
-                {
-                    type: "localeDropdown",
-                    position: "right",
-                },
+                // {
+                //     type: "localeDropdown",
+                //     position: "right",
+                // },
             ],
         },
 
