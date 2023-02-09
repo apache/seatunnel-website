21db46637040325d4bd26cc85e6a7e87daec229e
tungl <1778651752@qq.com>
2023-02-09 15:06:04 +0800

    [Feat] Add source, sink and transforms on the home page. (#205)
    * [Feat] Add source, sink and transforms on the home page.
    
    * [Feat] Update home page.

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -4,6 +4,30 @@ import useBaseUrl from '@docusaurus/useBaseUrl';
 import config from './languages.json';
 import './index.less';
 import systemConfiguration from '../../js/sysConfig'
+import Airtable from './img/Airtable.svg'
+import AppStore from './img/AppStore.svg'
+import Elasticsearch from './img/Elasticsearch.svg'
+import S3 from './img/S3.svg'
+import Clockify from './img/Clockify.svg'
+import HubSpot from './img/HubSpot.svg'
+import BingAds from './img/BingAds.svg'
+import MongoDB from './img/MongoDB.svg'
+import MySQL from './img/MySQL.svg'
+import PostgreSQL from './img/PostgreSQL.svg'
+import SalesForce from './img/SalesForce.svg'
+import TiDB from './img/TiDB.svg'
+import Redshift from './img/Redshift.svg'
+import Kyriba from './img/Kyriba.svg'
+import Flexport from './img/Flexport.svg'
+import ClickHouse from './img/ClickHouse.svg'
+import Courier from './img/Courier.svg'
+import DynamoDB from './img/DynamoDB.svg'
+import Firebase from './img/Firebase.svg'
+import ConvertKit from './img/ConvertKit.svg'
+import GitLab from './img/GitLab.svg'
+import Jira from './img/Jira.svg'
+import CoinApi from './img/CoinApi.svg'
+import Convex from './img/Convex.svg'
 
 const versions = require('../../../versions.json');
 
@@ -47,29 +71,121 @@ export default function () {
                                 <span>Slack</span>
                             </a>
                         </div>
-
-                        <div>
-                            <div className="cover_bg"></div>
-                            <div className="cover_page"/>
-                        </div>
-
-                        <div className="wave_wrapper wave_animation">
-                            <div className="wave_wrapper_inner bg_top"><div className="wave wave_top"></div></div>
-                            <div className="wave_wrapper_inner bg_middle"><div className="wave wave_middle"></div></div>
-                            <div className="wave_wrapper_inner bg_bottom"><div className="wave wave_bottom"></div></div>
-                        </div>
                     </div>
                 </div>
-
             </div>
 
-            <div className="main architecture">
-                <div className="block">
-                    <h1 className="main_subtitle text_center">{dataSource.common.architecture}</h1>
-                    <div className="item_block" style={{position: 'relative'}}>
-                        <p className="paragraph">{dataSource.home.architecture.desc}</p>
-                        <div className="architecture-image">
-                            <img className="button_icon github1" src={useBaseUrl('/image/seatunnel-workflow.svg')} alt="System Architecture"/>
+            <div className="connector">
+                <h1>Architecture Design</h1>
+                <p>
+                    <span>Currently Supported Quantity</span>
+                    <span>100+</span>
+                </p>
+                <div>
+                    <div className="connector_box">
+                        <h3>Source</h3>
+                        <div className="connector_row">
+                            <div>
+                                <MySQL />
+                            </div>
+                            <div>
+                                <PostgreSQL />
+                            </div>
+                            <div>
+                                <BingAds />
+                            </div>
+                        </div>
+                        <div className="connector_row">
+                            <div>
+                                <MongoDB />
+                            </div>
+                            <div>
+                                <Elasticsearch />
+                            </div>
+                            <div>
+                                <TiDB />
+                            </div>
+                        </div>
+                        <div className="connector_row">
+                            <div>
+                                <S3 />
+                            </div>
+                            <div>
+                                <HubSpot />
+                            </div>
+                            <div>
+                                <SalesForce />
+                            </div>
+                        </div>
+                        <div className="connector_row">
+                            <div>
+                                <Airtable />
+                            </div>
+                            <div>
+                                <AppStore />
+                            </div>
+                            <div>
+                                <Clockify />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="connector_transforms">
+                        <h3>Transforms</h3>
+                        <div>
+                            <div>Copy</div>
+                            <div>FieldMapper</div>
+                            <div>FilterRowKind</div>
+                            <div>Filter</div>
+                            <div>Replace</div>
+                            <div>Split</div>
+                            <div onClick={() => window.location.href = 'https://seatunnel.apache.org/docs/Connector-v2-release-state'}>Show More</div>
+                        </div>
+                    </div>
+                    <div className="connector_box">
+                        <h3>Sink</h3>
+                        <div className="connector_row">
+                            <div>
+                                <Redshift />
+                            </div>
+                            <div>
+                                <Jira />
+                            </div>
+                            <div>
+                                <Kyriba />
+                            </div>
+                        </div>
+                        <div className="connector_row">
+                            <div>
+                                <CoinApi />
+                            </div>
+                            <div>
+                                <ClickHouse />
+                            </div>
+                            <div>
+                                <Courier />
+                            </div>
+                        </div>
+                        <div className="connector_row">
+                            <div>
+                                <Convex />
+                            </div>
+                            <div>
+                                <Firebase />
+                            </div>
+                            <div>
+                                <ConvertKit />
+                            </div>
+                        </div>
+                        <div className="connector_row">
+                            <div>
+                                <Flexport />
+                            </div>
+                            <div>
+                                <DynamoDB />
+                            </div>
+                            <div>
+                                <GitLab />
+                            </div>
                         </div>
                     </div>
                 </div>
