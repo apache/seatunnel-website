a86502cdaa76dac1b130242c1f61be1eed832a8e
gaojun <gaojun2048@gmail.com>
2023-10-16 20:09:15 +0800

    Add seatunnel web document

---
+++ seatunnel_web_versioned_docs/version-1.0.0/about.md
@@ -0,0 +1,3 @@
+# About SeaTunnel Web
+
+SeaTunnel Web is a web subsystem of SeaTunnel, designed to provide an user-friendly operating system to help users better use SeaTunnel.
--- src/pages/download/index.jsx
+++ src/pages/download/index.jsx
@@ -1,60 +1,96 @@
 import React from 'react';
 import './index.less';
 import Layout from '@theme/Layout';
-import data from './data.json'
+import data from './st_data.json'
+import st_web_data from './st_web_data.json'
 
 const getLastPath = path => {
-	if (!path) { return '' }
+    if (!path) { return '' }
 
-	return path.substring(path.lastIndexOf('/') + 1)
+    return path.substring(path.lastIndexOf('/') + 1)
 }
 
 export default function () {
-	return (
-		<Layout>
-			<div className="main">
-				<div>
-					<h1>Download the SeaTunnel releases</h1>
-					<p>Use the links below to download the Apache SeaTunnel from one of our mirrors.</p>
-					<h2>SeaTunnel</h2>
-					<table className="version-table">
-						<thead>
-						<tr>
-							<th>Date</th>
-							<th>Version</th>
-							<th>Binary Distribution</th>
-							<th>Source Code</th>
-						</tr>
-						</thead>
-						<tbody>
-						{
-							data.map(item => {
-								return (
-									<tr key={item.version}>
-										<td>{item.date}</td>
-										<td>{item.version}</td>
-										<td>
-											<a href={item.binaryDistribution.bin}>[bin] {getLastPath(item.binaryDistribution.bin)}</a>
-											<a href={item.binaryDistribution.asc}>[asc] {getLastPath(item.binaryDistribution.asc)}</a>
-											<hr />
-											<a href={item.binaryDistribution.sha512}>[sha512] {getLastPath(item.binaryDistribution.sha512)}</a>
-										</td>
-										<td>
-											<a href={item.sourceCode.src}>[src] {getLastPath(item.sourceCode.src)}</a>
-											<a href={item.sourceCode.asc}>[asc] {getLastPath(item.sourceCode.asc)}</a>
-											<hr />
-											<a href={item.sourceCode.sha512}>[sha512] {getLastPath(item.sourceCode.sha512)}</a>
-										</td>
-									</tr>
-								)
-							})
-						}
-						</tbody>
-					</table>
-					<h2>Historical versions of non-Apache</h2>
-					<a href="https://github.com/apache/incubator-seatunnel/releases">Historical versions</a>
-				</div>
-			</div>
-		</Layout>
-	);
+    return (
+        <Layout>
+            <div className="main">
+                <div>
+                    <h1>Download the SeaTunnel releases</h1>
+                    <p>Use the links below to download the Apache SeaTunnel from one of our mirrors.</p>
+                    <h2>SeaTunnel</h2>
+                    <table className="version-table">
+                        <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Version</th>
+                            <th>Binary Distribution</th>
+                            <th>Source Code</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            data.map(st_item => {
+                                return (
+                                    <tr key={st_item.version}>
+                                        <td>{st_item.date}</td>
+                                        <td>{st_item.version}</td>
+                                        <td>
+                                            <a href={st_item.binaryDistribution.bin}>[bin] {getLastPath(st_item.binaryDistribution.bin)}</a>
+                                            <a href={st_item.binaryDistribution.asc}>[asc] {getLastPath(st_item.binaryDistribution.asc)}</a>
+                                            <hr />
+                                            <a href={st_item.binaryDistribution.sha512}>[sha512] {getLastPath(st_item.binaryDistribution.sha512)}</a>
+                                        </td>
+                                        <td>
+                                            <a href={st_item.sourceCode.src}>[src] {getLastPath(st_item.sourceCode.src)}</a>
+                                            <a href={st_item.sourceCode.asc}>[asc] {getLastPath(st_item.sourceCode.asc)}</a>
+                                            <hr />
+                                            <a href={st_item.sourceCode.sha512}>[sha512] {getLastPath(st_item.sourceCode.sha512)}</a>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                        </tbody>
+                    </table>
+                    <h2>SeaTunnel Web</h2>
+                    <table className="version-table">
+                        <thead>
+                        <tr>
+                            <th>Date</th>
+                            <th>Version</th>
+                            <th>Binary Distribution</th>
+                            <th>Source Code</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        {
+                            st_web_data.map(st_web_item => {
+                                return (
+                                    <tr key={st_web_item.version}>
+                                        <td>{st_web_item.date}</td>
+                                        <td>{st_web_item.version}</td>
+                                        <td>
+                                            <a href={st_web_item.binaryDistribution.bin}>[bin] {getLastPath(st_web_item.binaryDistribution.bin)}</a>
+                                            <a href={st_web_item.binaryDistribution.asc}>[asc] {getLastPath(st_web_item.binaryDistribution.asc)}</a>
+                                            <hr />
+                                            <a href={st_web_item.binaryDistribution.sha512}>[sha512] {getLastPath(st_web_item.binaryDistribution.sha512)}</a>
+                                        </td>
+                                        <td>
+                                            <a href={st_web_item.sourceCode.src}>[src] {getLastPath(st_web_item.sourceCode.src)}</a>
+                                            <a href={st_web_item.sourceCode.asc}>[asc] {getLastPath(st_web_item.sourceCode.asc)}</a>
+                                            <hr />
+                                            <a href={st_web_item.sourceCode.sha512}>[sha512] {getLastPath(st_web_item.sourceCode.sha512)}</a>
+                                        </td>
+                                    </tr>
+                                )
+                            })
+                        }
+                        </tbody>
+                    </table>
+                    <h2>Historical versions of non-Apache</h2>
+                    <a href="https://github.com/apache/incubator-seatunnel/releases">Historical versions</a>
+                </div>
+            </div>
+        </Layout>
+    );
 }
