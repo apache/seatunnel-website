acc56e8e87fbd2758b4cb202725dce5f624be5ff
Jia Fan <fanjiaeminem@qq.com>
2024-09-03 14:53:41 +0800

    [Improve] Remove mentor tab cause SeaTunnel is TLP (#328)

--- src/pages/team/index.js
+++ src/pages/team/index.js
@@ -14,29 +14,12 @@ export default function () {
             <div className="block team_page">
                 <h3 className="team_title">SeaTunnel Team</h3>
                 <p className="team_desc" dangerouslySetInnerHTML={ { __html: dataSource.info.desc } }/>
-                <h3 className="team_title">Mentor</h3>
-                <p className="team_desc">{dataSource.info.tip}</p>
-                <ul className="character_list">
-                    {
-                        dataSource.mentor.map((item, i) => (
-                            <a href={'https://github.com/' + item.githubId} key={i} target="_blank">
-                                <li className="character_item text_center" style={{'listStyle': 'none'}}>
-                                    <img className="character_avatar" src={item.avatarUrl} alt={item.name}/>
-                                    <div className="character_desc">
-                                        <h3 className="character_name">{item.name}</h3>
-                                        <h3 className="character_id"><span className="githubId">githubId:</span>{item.githubId}</h3>
-                                    </div>
-                                </li>
-                            </a>
-                        ))
-                    }
-                </ul>
 
                 <h3 className="team_title">PMC</h3>
                 <p className="team_desc">{dataSource.info.tip}</p>
                 <ul className="character_list">
                     {
-                        dataSource.pmc.map((item, i) => (
+                        config.pmc.map((item, i) => (
                             <a href={'https://github.com/' + item.githubId} key={i} target="_blank">
                                 <li className="character_item text_center" style={{'listStyle': 'none'}}>
                                     <img className="character_avatar" src={item.avatarUrl} alt={item.name}/>
@@ -54,7 +37,7 @@ export default function () {
                 <p className="team_desc">{dataSource.info.tip}</p>
                 <ul className="character_list">
                   {
-                    dataSource.committer.map((item, i) => (
+                      config.committer.map((item, i) => (
                       <a href={'https://github.com/' + item.githubId} key={i} target="_blank">
                         <li className="character_item text_center" style={{'listStyle': 'none'}}>
                           <img className="character_avatar" src={item.avatarUrl} alt={item.name}/>
