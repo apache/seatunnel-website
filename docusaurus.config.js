571a1bcedd2e8f8ad3a493b69e741f4400ba7851
Jia Fan <fanjiaeminem@qq.com>
2024-10-08 10:04:47 +0800

    [Improve] Add matomo integration for website analytics

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,17 +37,17 @@ const config = {
       <!-- Matomo -->
       innerHTML: `
           var _paq = window._paq = window._paq || [];
-          /* We explicitly disable cookie tracking to avoid privacy issues */
-          _paq.push(['disableCookies']);
           /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(["setDoNotTrack", true]);
+          _paq.push(["disableCookies"]);
           _paq.push(['trackPageView']);
           _paq.push(['enableLinkTracking']);
           (function() {
-              var u="https://analytics.apache.org/";
-              _paq.push(['setTrackerUrl', u+'matomo.php']);
-              _paq.push(['setSiteId', '30']);
-              var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-              g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+            var u="https://analytics.apache.org/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '65']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
           })();
         `,
     },
