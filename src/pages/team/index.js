cbd159b8f70d4421cc946113f26f33dda569ead4
DS <escheduler@outlook.com>
2024-11-29 19:44:50 +0800

    Update index.js

--- src/pages/team/index.js
+++ src/pages/team/index.js
@@ -42,7 +42,6 @@ export default function () {
                         <li className="character_item text_center" style={{'listStyle': 'none'}}>
                           <img className="character_avatar" src={item.avatarUrl} alt={item.name}/>
                           <div className="character_desc">
-                            <h3 className="character_name">{item.name}</h3>
                             <h3 className="character_id"><span className="githubId">githubId:</span>{item.githubId}</h3>
                           </div>
                         </li>
