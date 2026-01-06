c8d55908a609a7a25225d7f57257862a6a7c73f5
dailidong <dailidong66@gmail.com>
2026-01-06 16:29:06 +0800

    feat: add kapa.ai widget to homepage head

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,18 +3,19 @@ import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Home from './home'
 import useIsBrowser from '@docusaurus/useIsBrowser';
+import Head from '@docusaurus/Head';
 
-export default function() {
+export default function () {
     const isBrowser = useIsBrowser();
-    const {siteConfig} = useDocusaurusContext();
+    const { siteConfig } = useDocusaurusContext();
 
     const pathname = isBrowser && location.pathname
 
     useEffect(() => {
         if (isBrowser) {
             const nav = document.getElementsByTagName('nav')[0];
-            const classList = nav &&  nav.classList;
-            if(!classList) return;
+            const classList = nav && nav.classList;
+            if (!classList) return;
             if (pathname === '/' || pathname === '/zh-CN/') {
                 classList.add('index-nav');
             } else {
@@ -27,8 +28,11 @@ export default function() {
         <Layout
             title={siteConfig.title}
             description="Description will go into a meta tag in <head />">
+            <Head>
+                <script async src="https://widget.kapa.ai/kapa-widget.bundle.js" data-website-id="7a9c6ef2-bbf5-48d8-8711-66bfa6675033" data-project-name="Apache SeaTunnel" data-project-logo="https://seatunnel.apache.org/image/logo.png" data-project-color="#444FD9" data-modal-disclaimer="This is a custom LLM with access to all [SeaTunnel documentation](https://seatunnel.apache.org/docs/about)." data-consent-required data-consent-screen-disclaimer="By clicking &amp;quot;I agree, let's chat&amp;quot;, you consent to the use of the AI assistant in accordance with kapa.ai's [Privacy Policy](https://www.kapa.ai/content/privacy-policy). This service uses reCAPTCHA, which requires your consent to Google's [Privacy Policy](https://policies.google.com/privacy) and [Terms of Service](https://policies.google.com/terms). By proceeding, you explicitly agree to both kapa.ai's and Google's privacy policies." data-bot-protection-mechanism="hcaptcha"></script>
+            </Head>
             <main>
-                <Home/>
+                <Home />
             </main>
         </Layout>
     );
